7b23c3c33b919bff53174a33f74c708d77cd14fb
Abdulnasir-w <Abdulnasir-w@users.noreply.github.com>
2024-02-02 20:34:23 +0500

    initial

---
+++ lib/Config/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+//Background Color
+const bgColor = Color(0xffD1D9E6);
+
+//Button Color
+const primaryBtnColor = Color(0xff246AFE);
+const secondryBtnColor = Color(0xffFFFFFF);
+
+//Container Color
+const containerColor = Color(0xffFFFFFF);
+
+//Text Color
+const primaryTextColor = Color(0xffFFFFFF);
+const secondryTextColor = Color(0xff246AFE);
+const textColor = Color(0xff8C8C8C);

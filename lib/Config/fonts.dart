90e9e12f7aaacba6c5a3dfed0bdde764fc68061c
Abdulnasir-w <Abdulnasir-w@users.noreply.github.com>
2024-02-04 02:34:43 +0500

    file destination change

--- lib/Config/fonts.dart
+++ lib/Config/fonts.dart
@@ -1,3 +1,4 @@
+import 'package:bmi/Config/colors.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
@@ -5,7 +6,7 @@ TextStyle myStyle = GoogleFonts.oswald(
   textStyle: const TextStyle(
     color: Color(0xff8C8C8C),
     fontWeight: FontWeight.w500,
-    fontSize: 15,
+    fontSize: 20,
   ),
 );
 
@@ -22,5 +23,26 @@ TextStyle secondryStyle = GoogleFonts.oswald(
     color: Colors.black,
     fontWeight: FontWeight.w600,
     fontSize: 64,
+  ),
+);
+TextStyle btnTextStyle = GoogleFonts.oswald(
+  textStyle: const TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  ),
+);
+TextStyle primarybtnTextStyle = GoogleFonts.oswald(
+  textStyle: const TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+  ),
+);
+TextStyle secondrybtnTextStyle = GoogleFonts.oswald(
+  textStyle: const TextStyle(
+    color: primaryBtnColor,
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
   ),
 );
---
+++ lib/Model/collect_values.dart
@@ -0,0 +1,13 @@
+class DataModel {
+  final int weight;
+  final int age;
+  final double height;
+  final String gender;
+
+  DataModel({
+    required this.weight,
+    required this.age,
+    required this.height,
+    required this.gender,
+  });
+}
